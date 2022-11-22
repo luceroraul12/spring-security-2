0c1c88ac80f9240935dbdad6dad60a143229d28c
Lucero Raul <luceroraul12@gmail.com>
2022-11-22 18:04:04 -0300

    first commit

---
+++ src/test/java/luceroraul/spring/security2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package luceroraul.spring.security2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
