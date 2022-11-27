ff479a44ef499c610170a7cc6d36ee3602519cc0
Lucero Raul <luceroraul12@gmail.com>
2022-11-27 07:24:52 -0300

    add annotation and properties for loggin security in console

--- src/main/java/luceroraul/spring/security2/Application.java
+++ src/main/java/luceroraul/spring/security2/Application.java
@@ -3,8 +3,10 @@ package luceroraul.spring.security2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class Application {
 
 	public static void main(String[] args) {
